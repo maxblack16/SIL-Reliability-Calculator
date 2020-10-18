7b49cb2de8492f0c2bde7d19b0d8f15fba943d97
maxblack16 <72091966+maxblack16@users.noreply.github.com>
2020-10-18 10:03:24 +0100

    Update SILcalc.py
    Try and Except error correction.
    Added subsystem % contribution.

--- SILcalc.py
+++ SILcalc.py
@@ -1,15 +1,20 @@
-#Title:SIF Calc
+# Title:SIF Calc
 #
-#Description:
-##Calculates SIF Total PFDavg and RRF for most common voting configurations.
-##Voted subsystems are limited to having the same devices i.e. same failure rate data, prrof test interval, etc.
+# Description:
+# Calculates SIF Total PFDavg and RRF for most common voting configurations.
+# Voted subsystems are limited to having the same devices i.e. same failure rate data, prrof test interval, etc.
+# Calculations are for Low Demand functions.
 #
-#Written by: Matthew Bates
-#Date: 2nd September 2020
+# Written by: Matthew Bates
+# Date: 2nd September 2020
 #
+# Initialise variables:
 pfd = 0
 hft = 0
+vote = 0
 hours_per_year = 8760
+
+# Custom Functions:
 def results(pfd, hft):
     print('%s PFDavg = %g' % (subsystem, pfd))
     print('%s HFT = %d' % (subsystem, hft))
@@ -49,9 +54,12 @@ def vote_2oo3(subsystem, du_failure_rate_yr, proof_test_yr, mttr, dd_failure_rat
     hft = 1
     results(pfd, hft)
 
-def subcalc(voting):
+def subcalc(vote):
     while True:
         try:
+            global voting
+            vote = input('Enter voting (1oo1, 1oo2, 1oo3, 2oo2, 2oo3): ')
+            voting = vote
             if voting == '1oo1': #ISA-TR84.00.02-2015 Table C.2-1oo1
                 du_failure_rate_yr = float(input('Enter dangerous undiagnosed failure rate per year: '))
                 proof_test_yr = float(input('Enter proof test interval in years: '))
@@ -96,33 +104,29 @@ def subcalc(voting):
         except:
             print('Invalid input. Please try again.')
 
+# Main code:
 sif_name = input('Enter SIF name: ')
 sif_name = sif_name.upper()
 #%%
 print('\nSubsystem: Sensor') #Start sensor subsytem calcs
 subsystem = "Sensor"
-voting = input('Enter voting (1oo1, 1oo2, 1oo3, 2oo2, 2oo3): ')
-subcalc(voting)
+subcalc(vote)
 sensor_pfd = pfd
 sensor_hft = hft
-print(sensor_pfd, sensor_hft)
 #%%
 print('\nSubsystem: Logic Solver') #Start logic solver subsytem calcs
 subsystem = "Logic Solver"
-voting = input('Enter voting (1oo1, 1oo2, 1oo3, 2oo2, 2oo3): ')
-subcalc(voting)
+subcalc(vote)
 ls_pfd = pfd
 ls_hft = hft
-print(pfd)
 #%%
 print('\nSubsystem: Final Element') #Start final element subsytem calcs
 subsystem = "Final Element"
-voting = input('Enter voting (1oo1, 1oo2, 1oo3, 2oo2, 2oo3): ')
-subcalc(voting)
+subcalc(vote)
 fe_pfd = pfd
 fe_hft = hft
 #%%
-#Calculate Total PFDavg and RRF
+# Calculate Total PFDavg and RRF:
 sif_pfd = sensor_pfd + ls_pfd + fe_pfd
 sif_rrf = 1 / sif_pfd
 if 10 > sif_rrf:
@@ -136,7 +140,11 @@ elif 1000 <= sif_rrf <10000:
 else:
 	sil = 'Greater than SIL3, review data validity'
 
+sensor_pcnt = (sensor_pfd/sif_pfd)*100
+ls_pcnt = (ls_pfd/sif_pfd)*100
+fe_pcnt = (fe_pfd/sif_pfd)*100
 print('\nSIF: %s \nSIF Total PFDavg: %g \nSIF Total RRF: %g \nSIL: %s\n\nSensor HFT: %d \nLogic Solver HFT: %d \nFinal Element HFT: %d' %(sif_name, round(sif_pfd, 6), round(sif_rrf, 2), sil, sensor_hft, ls_hft, fe_hft))
+print('\nSensor contribution: %g%%\nLogic Solver contribution: %g%%\nFinal ELement contribution: %g%%' % (round(sensor_pcnt, 2), round(ls_pcnt, 2), round(fe_pcnt, 2)))
 #%%
 fout = open(sif_name+'.txt','w')
 print(fout)
